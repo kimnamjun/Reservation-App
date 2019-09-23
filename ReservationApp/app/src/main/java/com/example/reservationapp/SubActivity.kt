a5977afcb030beddcf5982cc7e669d6afb9b351b
Jeong Hyeongi <jhk5516@naver.com>
2019-09-24 00:51:51 +0900

    kotlin 파일 추가

---
+++ ReservationApp/app/src/main/java/com/example/reservationapp/SubActivity.kt
@@ -0,0 +1,13 @@
+package com.example.reservationapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+    }
+}
