3bf07b302a7107b580493f688bd922bf782c8f59
Jeong Hyeongi <jhk5516@naver.com>
2019-09-26 17:26:09 +0900

    Add files via upload
    - 메인화면에 '예약확인' 버튼 추가
    - Activity_confirm을 새로 만들어 '예약확인' 페이지 추가

--- ReservationApp/app/src/main/java/com/example/reservationapp/MainActivity.kt
+++ ReservationApp/app/src/main/java/com/example/reservationapp/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.reservationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button : ImageButton = findViewById(R.id.newActivity)
+        button.setBackgroundResource(R.drawable.hand)
+
+        var button_confirm : Button = findViewById(R.id.button_confirm)
+
+        button.setOnClickListener {
+            val nextIntent : Intent = Intent(this, SubActivity::class.java)
+            startActivity(nextIntent)
+        }
+
+        button_confirm.setOnClickListener {
+            val nextIntent : Intent = Intent(this, Activity_confirm::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
