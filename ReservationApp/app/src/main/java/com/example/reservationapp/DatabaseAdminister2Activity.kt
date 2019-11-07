f8b40d0f6337a6f0be91b2d7f7c59322008de31c
vxqwe0 <vxqwe0@gmail.com>
2019-11-07 18:28:01 +0900

    임시 병합

--- ReservationApp/app/src/main/java/com/example/reservationapp/DatabaseAdminister2Activity.kt
+++ ReservationApp/app/src/main/java/com/example/reservationapp/DatabaseAdminister2Activity.kt
@@ -23,7 +23,7 @@ class DatabaseAdminister2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_database_administer)
+        setContentView(R.layout.activity_database_administer2)
 
         var myRef1 = database.getReference("ReservationList")
         var myRef2 = database.getReference("OrderList")
