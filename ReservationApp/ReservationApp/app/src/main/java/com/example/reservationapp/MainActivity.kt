945e4e33a98beca4d9695e52cbd544177d72af3a
vxqwe0 <vxqwe0@gmail.com>
2019-11-18 21:32:22 +0900

    overwirte

---
+++ ReservationApp/ReservationApp/app/src/main/java/com/example/reservationapp/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.example.reservationapp
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.reservationapp.DbStructure.UserInfo
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    val mAuth = FirebaseAuth.getInstance()
+    var user = mAuth.currentUser
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val channelId = getString(R.string.default_notification_channel_id)
+            val channelName = getString(R.string.default_notification_channel_name)
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(NotificationChannel(channelId,
+                channelName, NotificationManager.IMPORTANCE_LOW))
+        }
+        // [START handle_data_extras]
+        intent.extras?.let {
+            for (key in it.keySet()) {
+                val value = intent.extras?.get(key)
+                Log.d(TAG, "Key: $key Value: $value")
+            }
+        }
+        // [END handle_data_extras]
+
+        Log.d(TAG, "Subscribing to reservation topic")
+        // [START subscribe_topics]
+        FirebaseMessaging.getInstance().subscribeToTopic("reservation")
+            .addOnCompleteListener { task ->
+                var msg = getString(R.string.msg_subscribed)
+                if (!task.isSuccessful) {
+                    msg = getString(R.string.msg_subscribe_failed)
+                }
+                Log.d(TAG, msg)
+                // Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            }
+        // [END subscribe_topics]
+
+        // Get token
+        // [START retrieve_current_token]
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = getString(R.string.msg_token_fmt, token)
+                Log.d(TAG, msg)
+                // Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
+        // [END retrieve_current_token]
+
+        newActivity.setOnClickListener{
+            user = mAuth.currentUser
+            if (user != null) {
+                val intent = Intent(this, QrReservationActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                Toast.makeText(baseContext, "로그인이 필요한 기능입니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        button_login.setOnClickListener {
+            user = mAuth.currentUser
+            if (user != null) {
+                FirebaseAuth.getInstance().signOut()
+                textView_showEmail.text = "사용자 정보 없음"
+                button_login.text = "로그인"
+                Toast.makeText(baseContext, "로그아웃 하였습니다.", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        user = mAuth.currentUser
+        var id : String?
+
+        if (user != null) {
+            button_login.text = "로그아웃"
+            id = user!!.email;
+            textView_showEmail.text = id
+        }
+        else{
+            button_login.text = "로그인"
+            textView_showEmail.text = "사용자 정보 없음"
+        }
+    }
+
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
+
---
+++ ReservationApp/ReservationApp/app/src/main/java/com/example/reservationapp/etc/DatabaseTestActivity.kt
@@ -0,0 +1,110 @@
+//package com.example.reservationapp
+//
+//import android.os.Bundle
+//import android.util.Log
+//import android.widget.Toast
+//import androidx.appcompat.app.AppCompatActivity
+//import com.google.firebase.auth.FirebaseAuth
+//import com.google.firebase.database.DataSnapshot
+//import com.google.firebase.database.DatabaseError
+//import com.google.firebase.database.FirebaseDatabase
+//import com.google.firebase.database.ValueEventListener
+//import kotlinx.android.synthetic.main.activity_database_test.*
+//import kotlinx.android.synthetic.main.activity_main.*
+//import java.text.SimpleDateFormat
+//import java.util.*
+//
+//class DatabaseTestActivity : AppCompatActivity() {
+//
+//    val database = FirebaseDatabase.getInstance()
+//    val mAuth = FirebaseAuth.getInstance()
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_database_test)
+//
+//       /* var myRefExam = database.getReference("UserList")
+//        myRefExam.child("20191107143502").getValue(String::class.java);
+//        for((index,value) in myRefExam.child) {
+//        }
+//
+//        Toast.makeText(baseContext, "", Toast.LENGTH_SHORT).show()
+//*/
+//        var resID = "Rest01"
+//
+//        var resName : String = "Rest Name Default"
+//        var userID  : String = "No User Default"
+//        var resTime : String = getTime()
+//        var waitNum : String = "-1"
+//
+//        //userInfo는 정보를 담고 toString 한 거
+//        var userInfo : String
+//        var myRef = database.getReference(resID)
+//        var count = myRef.child("count").key?:"-1"
+//
+//        var user = mAuth.currentUser
+//
+//        if (user != null) {
+//            // Null이 될 수도?
+//            userID = user.email!!
+//        }
+//        else{
+//            userID = "No User"
+//        }
+//        textView_userIDedit.text = userID
+//
+//        // QR 코드를 찍으면 실행되는 함수라고 가정한다.
+//        button_DB_submit.setOnClickListener{
+//            if (user != null) {
+//                // Null이 될 수도?
+//                userID = user.email!!
+//            }
+//            else{
+//                userID = "No User"
+//            }
+//            resName = editText_resName.text.toString()
+//            resTime = getTime()
+//
+//            myRef = database.getReference(resID)
+//
+//            userInfo = ReservationInformation(resName, userID, resTime, -1).toString()
+//
+//            myRef.child("User ID").setValue(userID)
+//            myRef.child("Restaurant Name").setValue(resName)
+//            myRef.child("Reservation Time").setValue(resTime)
+//            myRef.child("Waiting Number").setValue(waitNum)
+//            myRef.child("UserInfo" + count.toInt()).setValue(userInfo)
+//        }
+//
+//        myRef.addValueEventListener(object : ValueEventListener {
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                userInfo = dataSnapshot.child("UserInfo01").getValue(String::class.java)?:"User Info toString"
+//
+//                myRef = database.getReference(resID)
+//
+//                textView_userID.text = userID
+//                textView_resName.text = resName
+//                textView_resTime.text = resTime
+//                textView_waitNum.text = userInfo
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                // Failed to read value
+//                Log.w("zxcv", "Failed to read value.", error.toException())
+//            }
+//        })
+//
+//    }
+//
+//    private fun getTime() : String{
+//        return SimpleDateFormat("yyyyMMddHHmmss").format(Date(System.currentTimeMillis()))
+//    }
+//
+//    fun regReservation(){
+//
+//    }
+//
+//    companion object {
+//        private const val TAG = "DatabaseTestActivity"
+//    }
+//}
